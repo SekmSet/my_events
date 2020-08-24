be3a9a84998f3bad0f271dac8ecfd7c8ff8aaf40
Beli <belinda-pro@outlook.com>
2020-08-24 18:08:47 +0200

    redux

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
 
+const store = createStore(
+
+);
 ReactDOM.render(
   <React.StrictMode>
     <App />
