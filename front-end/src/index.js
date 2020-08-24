7bfc06bac927a944e29287fa55da47da56b00e84
Beli <belinda-pro@outlook.com>
2020-08-24 18:35:19 +0200

    redux installation

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from "redux";
+import { Provider } from 'react-redux';
+import rootReducer from "./_reducers/rootReducer";
 
 const store = createStore(
-
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}><App /></Provider>,
   document.getElementById('root')
 );
 
